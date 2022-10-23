c4c0b14a3c3aab69c0030319fc6d0fa568037372
fly-body <1773617797@qq.com>
2022-10-23 10:32:54 +0800

    发现列表页完成

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-(function(){
-    
+(function () {
+    $(".J-home").click(function () {
+        window.location.href = "index.html"
+        console.log("index");
+    })
+    $(".J-find").click(function () {
+        window.location.href = "businessList.html"
+    })
+    $(".J-order").click(function () {
+        window.location.href = "order.html"
+    })
+    $(".J-me").click(function () {
+        window.location.href = "me.html"
+    })
 })();
